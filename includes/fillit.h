9566461167685224211518f9c56c63bd31ef245a
Thomas POTIER <tpotier@e2r13p8.42.fr>
2019-04-16 15:00:24 +0200

    sample + input test

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,13 +6,17 @@
 /*   By: tpotier <tpotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 16:47:41 by tpotier           #+#    #+#             */
-/*   Updated: 2019/04/15 16:54:11 by tpotier          ###   ########.fr       */
+/*   Updated: 2019/04/16 14:50:08 by tpotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 
-#include "libft.h"
+# include "libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <fcntl.h>
+# include <unistd.h>
 
 #endif
--- srcs/fillit.c
+++ srcs/fillit.c
@@ -6,7 +6,7 @@
 /*   By: tpotier <tpotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 16:51:39 by tpotier           #+#    #+#             */
-/*   Updated: 2019/04/15 17:08:32 by tpotier          ###   ########.fr       */
+/*   Updated: 2019/04/16 14:59:56 by tpotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,23 @@ void	fillit_usage(char **ac)
 	ft_putendl(" filename");
 }
 
+int		fillit_input(char *filename)
+{
+	char	buff[5];
+	size_t	s;
+	int		fd;
+
+	if ((fd = open(filename, O_RDONLY)) < 0)
+		return (0);
+	if ((s = read(fd, buff, 5)) <= 0)
+		return (0);
+	if (s != 5 || buff[4] != '\n')
+		return (0);
+	buff[4] = '\0';
+	ft_putendl(buff);
+	return (1);
+}
+
 int		main(int ac, char **av)
 {
 	if (ac != 2)
@@ -25,6 +42,7 @@ int		main(int ac, char **av)
 			fillit_usage(av);
 			return (0);
 	}
-	ft_putendl("ok");
+	if (!fillit_input(av[1]))
+		ft_putendl("Error");
 	return (0);
 }
