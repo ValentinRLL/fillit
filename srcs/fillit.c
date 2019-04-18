4d3208d573c95f967ca8d8f0fceb2a98ac85f413
Thomas POTIER <tpotier@e1r11p1.42.fr>
2019-04-18 16:12:17 +0200

    norme fillit.c

--- srcs/fillit.c
+++ srcs/fillit.c
@@ -6,7 +6,7 @@
 /*   By: tpotier <tpotier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 16:51:39 by tpotier           #+#    #+#             */
-/*   Updated: 2019/04/18 15:58:26 by tpotier          ###   ########.fr       */
+/*   Updated: 2019/04/18 16:11:38 by tpotier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	fillit_usage(char **av)
 	ft_putendl(" filename");
 }
 
-#include <stdio.h>
 int		main(int ac, char **av)
 {
 	t_piece	**pieces;
@@ -32,13 +31,6 @@ int		main(int ac, char **av)
 	{
 		ft_putendl("Error");
 		return (-1);
-	}
-	int	i = 0;
-	while (pieces[i])
-	{
-		printf("%s\n%s\n%s\n%s\n\n", pieces[i]->content[0],
-				pieces[i]->content[1], pieces[i]->content[2], pieces[i]->content[3]);
-		i++;
 	}
 	place(pieces);
 	return (0);
